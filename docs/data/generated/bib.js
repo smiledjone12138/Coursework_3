b239194d7d3633a097c136839cf4920b25ae67a0
smiledjone12138 <smiledjone@gmail.com>
2025-05-15 03:10:38 +0100

    Add files via upload

---
+++ docs/data/generated/bib.js
@@ -0,0 +1,111 @@
+﻿define({ entries : {
+    "Bourigault2024xdiffusion": {
+        "author": "Emilie Bourigault and Ahmed Hamdi and Adrian Jamaludin",
+        "journal": "arXiv preprint arXiv:2404.19604",
+        "keywords": "type:methodological, method:diffusion model, model:cross-sectional diffusion, task:MRI synthesis, data:medical image, evaluation:qualitative",
+        "title": "X-diffusion: Generating detailed 3D MRI volumes from a single image using cross-sectional diffusion models",
+        "type": "article",
+        "url": "https://arxiv.org/abs/2404.19604",
+        "year": "2024"
+    },
+    "Chung2022score": {
+        "author": "Hyeongwoo Chung and Jong Chul Ye",
+        "doi": "10.1016/j.media.2022.102479",
+        "journal": "Medical Image Analysis",
+        "keywords": "type:empirical, method:score-based diffusion model, task:MRI reconstruction, data:medical image, evaluation:quantitative",
+        "pages": "102479",
+        "title": "Score-based diffusion models for accelerated MRI",
+        "type": "article",
+        "volume": "80",
+        "year": "2022"
+    },
+    "Dorjsembe2024conditional": {
+        "author": "Zorigto Dorjsembe and Hong-Kai Pao and Sukhbaatar Odonchimed and Fangxiang Xiao",
+        "journal": "IEEE Journal of Biomedical and Health Informatics",
+        "keywords": "type:methodological, method:conditional diffusion model, task:semantic MRI synthesis, data:brain MRI, evaluation:qualitative",
+        "title": "Conditional diffusion models for semantic 3D brain MRI synthesis",
+        "type": "article",
+        "year": "2024"
+    },
+    "Golkov2016modelfree": {
+        "author": "Vladimir Golkov and Thomas Sprenger and Jens Sperl and Markus Menzel and Michael Czisch and Peter Samann and Daniel Cremers",
+        "booktitle": "2016 IEEE 13th International Symposium on Biomedical Imaging (ISBI)",
+        "keywords": "type:empirical, method:model-free diffusion MRI, task:novelty detection, data:diffusion MRI, evaluation:qualitative",
+        "organization": "IEEE",
+        "pages": "1233--1236",
+        "title": "Model-free novelty-based diffusion MRI",
+        "type": "inproceedings",
+        "year": "2016"
+    },
+    "Ianus2016model": {
+        "author": "Andrei Ianu\u015f and Ivan Drobnjak and Daniel C. Alexander",
+        "doi": "10.1002/nbm.3508",
+        "journal": "NMR in Biomedicine",
+        "keywords": "type:simulation, method:model-based diffusion MRI, task:anisotropy estimation, data:diffusion MRI, evaluation:simulation",
+        "number": "5",
+        "pages": "672--685",
+        "title": "Model-based estimation of microscopic anisotropy using diffusion MRI: a simulation study",
+        "type": "article",
+        "volume": "29",
+        "year": "2016"
+    },
+    "Lee2023improving": {
+        "author": "Seungjun Lee and Hyeongwoo Chung and Minjae Park and Jaejun Park and Woo Seok Ryu and Jong Chul Ye",
+        "booktitle": "Proceedings of the IEEE/CVF International Conference on Computer Vision",
+        "keywords": "type:empirical, method:2D diffusion models, task:3D MRI reconstruction, data:medical image, evaluation:quantitative",
+        "pages": "10710--10720",
+        "title": "Improving 3D imaging with pre-trained perpendicular 2D diffusion models",
+        "type": "inproceedings",
+        "year": "2023"
+    },
+    "Litrico2024tadm": {
+        "author": "Matteo Litrico and Fabrizio Guarnera and Marco V. Giuffrida and Davide Rav\u00ec and Salvatore Battiato",
+        "booktitle": "International Conference on Medical Image Computing and Computer-Assisted Intervention",
+        "keywords": "type:methodological, method:temporally-aware diffusion model, task:disease progression modeling, data:brain MRI, evaluation:qualitative",
+        "pages": "444--453",
+        "publisher": "Springer Nature Switzerland",
+        "title": "TADM: Temporally-aware diffusion model for neurodegenerative progression on brain MRI",
+        "type": "inproceedings",
+        "year": "2024"
+    },
+    "Lupke2024physicsinformed": {
+        "author": "Sebastian L\u00fcpke and Yeganeh Yeganeh and Ehsan Adeli and Nassir Navab and Alireza Farshad",
+        "booktitle": "International Conference on Medical Image Computing and Computer-Assisted Intervention",
+        "keywords": "type:methodological, method:latent diffusion, task:multimodal MRI synthesis, data:brain MRI, evaluation:qualitative",
+        "pages": "198--207",
+        "publisher": "Springer Nature Switzerland",
+        "title": "Physics-informed latent diffusion for multimodal brain MRI synthesis",
+        "type": "inproceedings",
+        "year": "2024"
+    },
+    "Mani2020modelbased": {
+        "author": "Mani P. Mani and Harsh Kumar Aggarwal and Saurabh Ghosh and Mathews Jacob",
+        "booktitle": "2020 IEEE 17th International Symposium on Biomedical Imaging (ISBI)",
+        "keywords": "type:empirical, method:model-based deep learning, task:MRI reconstruction, data:diffusion MRI, evaluation:quantitative",
+        "organization": "IEEE",
+        "pages": "913--916",
+        "title": "Model-based deep learning for reconstruction of joint kq under-sampled high resolution diffusion MRI",
+        "type": "inproceedings",
+        "year": "2020"
+    },
+    "Peng2023conditional": {
+        "author": "Wei Peng and Ehsan Adeli and Tim Bosschieter and Seong H. Park and Qi Zhao and Kilian M. Pohl",
+        "booktitle": "International Conference on Medical Image Computing and Computer-Assisted Intervention",
+        "keywords": "type:methodological, method:conditional diffusion probabilistic model, task:MRI synthesis, data:brain MRI, evaluation:qualitative",
+        "pages": "14--24",
+        "publisher": "Springer Nature Switzerland",
+        "title": "Generating realistic brain MRIs via a conditional diffusion probabilistic model",
+        "type": "inproceedings",
+        "year": "2023"
+    },
+    "Pinaya2022latent": {
+        "author": "Wouter H. Pinaya and Petru-Dan Tudosiu and Julien Dafflon and Pedro F. Da Costa and Victor Fernandez and Parashkev Nachev and Sebastien Ourselin and M. Jorge Cardoso",
+        "booktitle": "MICCAI Workshop on Deep Generative Models",
+        "keywords": "type:methodological, method:latent diffusion models, task:brain image generation, data:brain MRI, evaluation:qualitative",
+        "pages": "117--126",
+        "publisher": "Springer Nature Switzerland",
+        "title": "Brain imaging generation with latent diffusion models",
+        "type": "inproceedings",
+        "year": "2022"
+    }
+}});
